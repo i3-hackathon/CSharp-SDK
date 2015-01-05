498aaaf3d58911e9ad35e57232caf8773f4d2211
conneryn <conneryn@moj.io>
2015-01-05 04:27:40 -0800

    Fix references and Nuget package for BMW Client.

---
+++ Src/Mojio.Client/BMWClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mojio.Client
+{
+    public class BMWClient : MojioClient
+    {
+        /// <summary>
+        /// Initiate a new BMWClient.  No session token has been created yet.
+        /// Client must call BeginAsync, or set a valid Token.
+        /// </summary>
+        /// <param name="Url">API endpoint URL</param>
+        public BMWClient(string Url = Live) : base(Url)
+        {
+
+        }
+    }
+}
